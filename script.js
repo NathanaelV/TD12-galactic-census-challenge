8ec7cf35b0f611b22f57dd97675b2801c2ddacf4
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-04-04 17:56:58 -0300

    Get data from a planet

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
 let result = document.getElementById('result');
+let planet_details = document.getElementById('planet_details')
+
+window.onload = function() {
+    printData();
+};
 
 async function printData() {
     let res = await fetch('https://swapi.dev/api/planets');
 
     let {results} = await res.json();
-    // console.log(results)
 
     results.forEach(planet => {
-        let button = document.createElement('button');
-        button.innerHTML = planet.name
-        result.appendChild(button);
+        let li = document.createElement('div');
+        li.innerHTML = `<button onclick="printDataPlanet('${planet.url}')">${planet.name}</button>`;
+        result.appendChild(li);
     });
 }
+
+async function printDataPlanet(planet_url) {
+    let res = await fetch(planet_url);
+    let planet_data = await res.json();
+    console.log(planet_data)
+
+    planet_details.innerHTML = `
+        <strong>Está é o Planeta: ${planet_data.name}</strong><br>
+        Clima: ${planet_data.climate}<br>
+        Tem ${planet_data.population} habitantes<br>
+        Terreno: ${planet_data.terrain}<br>
+    `;
+};
